7821c06c3b593ff710370873c10252023d82dbeb
Filipe M. Santos <correio.filipe@gmail.com>
2024-04-29 16:07:42 -0300

    update projeto java-anatomia-classes

---
+++ java-anatomia-classes/src/edu/filipe/segundasemana/MinhaClasse.java
@@ -0,0 +1,19 @@
+package edu.filipe.segundasemana;
+
+public class MinhaClasse {
+
+    public static void main(String[] args) {
+
+        String primeiroNome = "Filipe";
+        String segundoNome = "Moreira dos Santos";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
+        return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
+    }
+
+}
