716fb152c828e27d46b17dc8563225beac879787
Filipe M. Santos <correio.filipe@gmail.com>
2024-04-29 16:58:36 -0300

    update projeto tipos-variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,27 @@
 public class TiposVariaveis {
+    /**
+     * @param args
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        int idade; // Tipo "int", nome "idade", com nenhum valor atribuído.
+        int anoFabricacao = 2021; // tipo "int", nome "anoFabricacao", com valor 2021.
+        double salarioMinimo = 2.500; // tipo "double", nome "salarioMinimo", valor 2.500.
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.print(numero);
+
+        final double VALOR_DE_PI = 3.14;
+
+        VALOR_DE_PI = 10.75; // Esta linha vai apresentar erro de compilação!
+
     }
 }
