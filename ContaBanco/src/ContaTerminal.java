ca19fe296e1afdd422586218262ccb5334528e80
Filipe M. Santos <correio.filipe@gmail.com>
2024-05-02 15:16:04 -0300

    commit CONTABANCO

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor digite o seu nome!");
+        String nome = scanner.nextLine();
+
+        System.out.println("Por favor digite sua agência");
+        String agencia = scanner.next();
+
+        System.out.println("Agora informe o numero da conta");
+        int numero = scanner.nextInt();
+
+        System.out.println("Informe o seu saldo");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia +
+                ", conta " + numero + " e seu saldo R$" + saldo + " já está disponível para saque");
+    }
+}
